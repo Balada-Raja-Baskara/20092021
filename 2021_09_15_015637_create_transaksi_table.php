4d371f52ca571e49eca1ed9745b9034335482e26
Balada-Raja-Baskara <balada_raja_29rpl@student.smktelkom-mlg.sch.id>
2021-09-20 15:41:53 +0700

    20092021

---
+++ 2021_09_15_015637_create_transaksi_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransaksiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksi', function (Blueprint $table) {
+            $table->bigIncrements('id_transaksi');
+            $table->unsignedBigInteger('id_petugas');
+            $table->unsignedBigInteger('id_pelanggan');
+            $table->date('tgl_transaksi');
+
+            $table->foreign('id_petugas')->references('id_petugas')->on('petugas');
+            $table->foreign('id_pelanggan')->references('id_pelanggan')->on('customers');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksi');
+    }
+}
